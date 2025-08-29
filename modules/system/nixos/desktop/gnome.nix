bb9b6ae7e23ee76bb1846638186e4f7027eba265
lspita <ludovico.spitaleri@gmail.com>
2025-08-29 18:22:19 +0200

    network manager wifi gnome fix

--- modules/system/nixos/desktop/gnome.nix
+++ modules/system/nixos/desktop/gnome.nix
@@ -19,6 +19,11 @@ customLib.mkModule {
         description = "Whether to enable gnome ssh agent";
       };
     };
+    networkManagerWifiFix = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to enable the network manager wifi fix (if using network manager)";
+    };
   };
   mkConfig =
     { cfg }:
@@ -27,5 +32,16 @@ customLib.mkModule {
         desktopManager.gnome.enable = true;
         gnome.gcr-ssh-agent.enable = cfg.sshAgent.enable;
       };
+      networking.networkmanager.settings =
+        lib.mkIf (cfg.networkManagerWifiFix && config.networking.networkmanager.enable)
+          {
+            # fix wifi considered without internet access
+            # https://discourse.nixos.org/t/is-gnome-supposed-to-detect-captive-portals/44417/4
+            # converted to settings attribute because extraConfig is not supported anymore
+            connectivity = {
+              uri = "http://google.cn/generate_204";
+              response = "";
+            };
+          };
     };
 }
