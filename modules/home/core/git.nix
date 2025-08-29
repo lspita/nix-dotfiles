1a838874d6530b4d016db420b82821982536271e
lspita <ludovico.spitaleri@gmail.com>
2025-08-29 10:51:54 +0200

    git single file config

---
+++ modules/home/core/git.nix
@@ -0,0 +1,54 @@
+{
+  config,
+  customLib,
+  lib,
+  pkgs,
+  vars,
+  ...
+}:
+customLib.mkModule {
+  inherit config;
+  path = [
+    "core"
+    "git"
+  ];
+  mkConfig =
+    { ... }:
+    let
+      uvars = vars.user;
+      attributesConfigPath = "git/attributes";
+    in
+    {
+      # https://github.com/ryan4yin/nix-config/blob/c56593516d8309557f4b74e60add8e2a36f0bf95/home/base/core/git.nix#L9
+      # `programs.git` will generate the config file: ~/.config/git/config
+      # to make git use this config file, `~/.gitconfig` should not exist!
+      #
+      #   https://git-scm.com/docs/git-config#Documentation/git-config.txt---global
+      home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+        rm -f ${config.home.homeDirectory}/.gitconfig
+      '';
+
+      programs.git = {
+        package = pkgs.git;
+        enable = true;
+        userName = uvars.username;
+        userEmail = uvars.email;
+        extraConfig = {
+          init.defaultBranch = "main";
+          pull.rebase = false;
+          core = {
+            editor = vars.editor;
+            attributesfile = "${config.xdg.configHome}/${attributesConfigPath}";
+          };
+        };
+      };
+      xdg.configFile = {
+        ${attributesConfigPath}.text = ''
+          * text=auto eol=lf
+          *.[cC][mM][dD] text eol=crlf
+          *.[bB][aA][tT] text eol=crlf
+          *.[pP][sS]1 text eol=crlf
+        '';
+      };
+    };
+}
