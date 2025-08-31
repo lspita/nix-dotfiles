fee91605b1ceb0f438351277e55c06f50950912e
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-08-31 22:24:27 +0200

    network manager assertion for gnome wifi fix

--- modules/system/nixos/desktop/gnome.nix
+++ modules/system/nixos/desktop/gnome.nix
@@ -28,20 +28,27 @@ customLib.mkModule {
   mkConfig =
     { cfg }:
     {
+      assertions = [
+        {
+          assertion = !cfg.networkManagerWifiFix || config.networking.networkmanager.enable;
+          message = "Cannot enable option networkManagerWifiFix if networkmanager is disabled.";
+        }
+      ];
+
       services = {
         desktopManager.gnome.enable = true;
         gnome.gcr-ssh-agent.enable = cfg.sshAgent.enable;
       };
       networking.networkmanager.settings =
-        lib.mkIf (cfg.networkManagerWifiFix && config.networking.networkmanager.enable)
-          {
-            # fix wifi considered without internet access
-            # https://discourse.nixos.org/t/is-gnome-supposed-to-detect-captive-portals/44417/4
-            # converted to settings attribute because extraConfig is not supported anymore
-            connectivity = {
-              uri = "http://google.cn/generate_204";
-              response = "";
-            };
+        # networkmanager is enabled because of assertion, but check anyways
+        lib.mkIf (cfg.networkManagerWifiFix && config.networking.networkmanager.enable) {
+          # fix wifi considered without internet access
+          # https://discourse.nixos.org/t/is-gnome-supposed-to-detect-captive-portals/44417/4
+          # converted to settings attribute because extraConfig is not supported anymore
+          connectivity = {
+            uri = "http://google.cn/generate_204";
+            response = "";
           };
+        };
     };
 }
