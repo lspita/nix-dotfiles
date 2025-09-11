c196df4679a0fc79939ff390e360ec5ad4b9d8d2
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-11 21:27:20 +0200

    overview search filter

--- modules/home/linux/desktop/plasma/settings.nix
+++ modules/home/linux/desktop/plasma/settings.nix
@@ -21,6 +21,7 @@ customLib.mkModule {
         input.keyboard.layouts = [ { layout = vars.linux.locale.keyboard; } ];
         session.sessionRestore.restoreOpenApplicationsOnLogin = "startWithEmptySession";
         configFile.kded5rc.Module-browserintegrationreminder.autoload = false; # disable browser integration alert in system tray
+        configFile.kwinrc.Effect-overview.FilterWindows = false; # make search in overview ignore windows
       };
     };
 }
