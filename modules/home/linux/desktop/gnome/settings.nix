a35cf591a24867fd95d4b1ef0d19e7a9a561b2d6
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-13 19:47:17 +0200

    gnome: night light

--- modules/home/linux/desktop/gnome/settings.nix
+++ modules/home/linux/desktop/gnome/settings.nix
@@ -2,6 +2,7 @@
   config,
   customLib,
   pkgs,
+  lib,
   ...
 }:
 customLib.mkModule {
@@ -20,6 +21,12 @@ customLib.mkModule {
         "org/gnome/desktop/interface".gtk-enable-primary-paste = false;
         "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
         "org/gnome/mutter".center-new-windows = true;
+        "org/gnome/settings-daemon/plugins/color" = {
+          # manual night light
+          night-light-schedule-from = 0.0;
+          night-light-schedule-to = 0.0;
+          night-light-temperature = lib.hm.gvariant.mkUint32 3200;
+        };
       };
     };
 }
