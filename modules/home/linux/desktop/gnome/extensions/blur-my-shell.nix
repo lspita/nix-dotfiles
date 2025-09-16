83d89695c9a9f02329d04b595e79363297f22084
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-16 10:54:10 +0200

    gnome: blur apps

--- modules/home/linux/desktop/gnome/extensions/blur-my-shell.nix
+++ modules/home/linux/desktop/gnome/extensions/blur-my-shell.nix
@@ -13,13 +13,29 @@ lib.custom.mkModule {
     "extensions"
     "blur-my-shell"
   ];
+  extraOptions = {
+    applications = {
+      enable = lib.custom.mkTrueEnableOption "applications blur";
+      blacklist = lib.mkOption {
+        type = with lib.types; listOf str;
+        default = [ ];
+        description = "Applications blur blacklisted apps";
+      };
+      opaqueFocused = lib.mkEnableOption "opaque effect for focused app";
+    };
+  };
   mkConfig =
-    { ... }:
+    { cfg }:
     lib.custom.gnome.mkExtensionConfig {
       package = pkgs.gnomeExtensions.blur-my-shell;
       settings = {
         "overview".style-components = 3; # transparent
         "panel".blur = false;
+        "applications" = with cfg.applications; {
+          inherit blacklist;
+          enable-all = enable;
+          dynamic-opacity = opaqueFocused;
+        };
       };
     };
 }
