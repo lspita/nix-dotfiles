2f916af1e19ef2b66d3671e4009cce60e522c2f1
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-20 10:44:02 +0200

    update + removed plasma manager

--- flake.nix
+++ flake.nix
@@ -24,12 +24,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
-
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
