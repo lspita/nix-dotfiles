648162f58392ba1d0d30a337dd6e6dc8367eb1ef
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-28 23:29:40 +0200

    bash: case-insensitive completition

--- modules/home/shell/bash/default.nix
+++ modules/home/shell/bash/default.nix
@@ -1,10 +1,22 @@
 { config, lib, ... }:
 with lib.custom;
 modules.mkModule config ./. {
-  config = {
-    programs.bash = {
-      enable = true;
-      enableCompletion = true;
+  options = {
+    completition = {
+      enable = utils.mkTrueEnableOption "bash completition";
+      ignoreCase = utils.mkTrueEnableOption "case-insensitive completition";
     };
   };
+  config =
+    { self, ... }:
+    {
+      programs.bash = {
+        enable = true;
+        enableCompletion = self.completition.enable;
+      };
+      # https://www.cyberciti.biz/faq/bash-shell-setup-filename-tab-completion-case-insensitive/
+      home.file.".inputrc".text = ''
+        ${if self.completition.ignoreCase then "set completion-ignore-case on" else ""}
+      '';
+    };
 }
