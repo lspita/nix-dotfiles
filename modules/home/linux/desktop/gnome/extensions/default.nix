cb82f0afa1c31fe0964b4d95770ce5d5fb62d93c
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-28 23:07:37 +0200

    gnome: hide cursor

--- modules/home/linux/desktop/gnome/extensions/default.nix
+++ modules/home/linux/desktop/gnome/extensions/default.nix
@@ -18,6 +18,7 @@ modules.mkDefaultsModule config ./. {
       open-bar.enable = true;
       alphabetical-app-grid.enable = true;
       auto-power-profile.enable = true;
+      hide-cursor.enable = true;
     })
     // {
       home.packages = with pkgs; [
