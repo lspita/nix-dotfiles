90a3de9c68885a2d4d2a88db29d5e23549ad412b
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-28 23:55:34 +0200

    bash: purge aliases

--- modules/home/shell/aliases.nix
+++ modules/home/shell/aliases.nix
@@ -1,10 +1,17 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  vars,
+  ...
+}:
 with lib.custom;
 modules.mkModule config ./aliases.nix {
   config = {
     home.shellAliases = {
       ll = "ls -al";
       la = "ls -a";
+      purge-all = "find . -mindepth 1 -maxdepth 1 -print -exec rm -rf '{}' \\;";
+      clean-home-backups = "find . -name '*.${vars.backupFileExtension}' -print -exec rm -rf '{}' \\;";
     };
   };
 }
