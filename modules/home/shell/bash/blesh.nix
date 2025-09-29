614390d1df7ebeebc0953d190dd2ca92d7e27d93
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-29 23:47:55 +0200

    blesh: follow docs source order

--- modules/home/shell/bash/blesh.nix
+++ modules/home/shell/bash/blesh.nix
@@ -8,12 +8,14 @@ with lib.custom;
 modules.mkModule config ./blesh.nix {
   config = {
     home.packages = with pkgs; [ blesh ];
-    custom.shell.rc = lib.mkAfter [
-      # https://github.com/akinomyoga/ble.sh/wiki/Manual-A1-Installation#user-content-nixpkgs
-      ''
-        source -- "$(blesh-share)"/ble.sh --attach=none # attach does not work currently
-        [[ ! \$\{BLE_VERSION-} ]] || ble-attach
-      ''
+    # https://github.com/akinomyoga/ble.sh/wiki/Manual-A1-Installation#user-content-nixpkgs
+    custom.shell.rc = lib.mkMerge [
+      (lib.mkBefore [
+        ''source -- "$(blesh-share)"/ble.sh --attach=none # attach does not work currently''
+      ])
+      (lib.mkAfter [
+        ''[[ ! \$\{BLE_VERSION-} ]] || ble-attach''
+      ])
     ];
   };
 }
