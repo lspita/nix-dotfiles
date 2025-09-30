e4e46aee1b327b4a70ae4780f8d36917862efdb0
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-09-30 18:03:41 +0200

    blesh attach

--- modules/home/shell/bash/blesh.nix
+++ modules/home/shell/bash/blesh.nix
@@ -11,7 +11,7 @@ modules.mkModule config ./blesh.nix {
     # https://github.com/akinomyoga/ble.sh/wiki/Manual-A1-Installation#user-content-nixpkgs
     custom.shell.rc = lib.mkMerge [
       (lib.mkBefore [
-        ''source -- "$(blesh-share)"/ble.sh --attach=none # attach does not work currently''
+        ''source -- "$(blesh-share)"/ble.sh''
       ])
       (lib.mkAfter [
         ''[[ ! \$\{BLE_VERSION-} ]] || ble-attach''
