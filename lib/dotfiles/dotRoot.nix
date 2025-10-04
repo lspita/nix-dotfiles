5e3af43b68d04e5a2a0d7d0910efbef913afa5de
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-10-04 14:15:57 +0000

    wrong dotroot path

--- lib/dotfiles/dotRoot.nix
+++ lib/dotfiles/dotRoot.nix
@@ -1,5 +1,3 @@
 { vars }:
 config:
-"${
-  config.home.homeDirectory or config.users.users.${vars.user.username}.home
-}//${vars.dotfilesHome}"
+"${config.home.homeDirectory or config.users.users.${vars.user.username}.home}/${vars.dotfilesHome}"
