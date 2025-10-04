0c2b3e23adc60edf0ea11b9c224f7163cc12d505
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-10-04 12:14:03 +0200

    no default apps in wsl

---
+++ hosts/ratchet-wsl/vars.nix
@@ -0,0 +1,22 @@
+{
+  linux.defaultApps = {
+    terminal = {
+      desktop = null;
+      program = null;
+    };
+    browser = {
+      desktop = null;
+      program = null;
+    };
+    editor = {
+      desktop = null;
+    };
+    fileManager = null;
+    music = null;
+    pdf = null;
+    mail = null;
+    image = null;
+    audio = null;
+    video = null;
+  };
+}
