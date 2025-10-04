7fc1901c22ee2dc5ca72ffb119ca7c0afdb38417
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-10-04 13:24:26 +0200

    nixos: no power management

--- modules/system/nixos/core/power.nix
+++ modules/system/nixos/core/power.nix
@@ -2,7 +2,6 @@
 with lib.custom;
 modules.mkModule config ./power.nix {
   config = {
-    powerManagement.enable = true;
     boot.kernelParams = [ "usbcore.autosuspend=-1" ]; # do not turn off fingerprint reader
     systemd.sleep.extraConfig = ''
       AllowHibernation=no
