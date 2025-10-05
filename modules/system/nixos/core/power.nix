0e8f3bf89877262ab3f22ba8fb6e0e5abed22e90
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-10-05 16:27:32 +0200

    power management

--- modules/system/nixos/core/power.nix
+++ modules/system/nixos/core/power.nix
@@ -2,6 +2,7 @@
 with lib.custom;
 modules.mkModule config ./power.nix {
   config = {
+    powerManagement.enable = true;
     boot.kernelParams = [ "usbcore.autosuspend=-1" ]; # do not turn off fingerprint reader
     systemd.sleep.extraConfig = ''
       AllowHibernation=no
