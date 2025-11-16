76e3d9aa6fd42bf9f4802e467c4c43b7ee194844
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-11-16 13:35:14 +0100

    zed: base configs in json files

---
+++ modules/home/apps/editor/zed/default.nix
@@ -0,0 +1,43 @@
+{ lib, vars, ... }@inputs:
+with lib.custom;
+modules.mkModule inputs ./. {
+  config = {
+    programs.zed-editor =
+      let
+        objectConfig =
+          path: override: lib.attrsets.recursiveUpdate (builtins.fromJSON (builtins.readFile path)) override;
+        listConfig = path: override: builtins.fromJSON (builtins.readFile path) ++ override;
+      in
+      {
+        enable = true;
+        extensions = [
+          "catppuccin"
+          "catppuccin-icons"
+          "git-firefly"
+          "nix"
+          "dockerfile"
+          "docker-compose"
+          "log"
+          "ssh-config"
+          "markdown-oxide"
+          "toml"
+          "neocmake"
+        ];
+        userSettings = objectConfig ./settings.json (
+          let
+            normalFont = vars.fonts.normal;
+            monoFont = vars.fonts.monospace;
+            fontZoom = 1.5;
+          in
+          {
+            ui_font_family = normalFont.name;
+            ui_font_size = normalFont.size * fontZoom;
+            buffer_font_family = monoFont.name;
+            buffer_font_size = monoFont.size * fontZoom;
+          }
+        );
+        userKeymaps = listConfig ./keymap.json [ ];
+      };
+    home.shellAliases.zed = "zeditor";
+  };
+}
