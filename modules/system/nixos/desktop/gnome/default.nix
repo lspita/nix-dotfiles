a999476e48ea770344a3f34ce0e047a9f7a9482b
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-11-18 18:02:40 +0100

    gnome: fix visual glitches in gtk popup windows

--- modules/system/nixos/desktop/gnome/default.nix
+++ modules/system/nixos/desktop/gnome/default.nix
@@ -21,16 +21,21 @@ modules.mkModule inputs ./. {
     in
     {
       services.desktopManager.gnome.enable = true;
-      environment.gnome.excludePackages =
-        with pkgs;
-        [
-          gnome-contacts
-          gnome-tour
-          geary
-          epiphany
-          gnome-console
-        ]
-        ++ self.excludePackages;
+      environment = {
+        gnome.excludePackages =
+          with pkgs;
+          [
+            gnome-contacts
+            gnome-tour
+            geary
+            epiphany
+            gnome-console
+          ]
+          ++ self.excludePackages;
+        # Fix visual glitches in gtk popup windows
+        # https://www.reddit.com/r/Fedora/comments/1oirfcr/comment/nlzxhyq/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+        sessionVariables.GSK_RENDERER = "gl";
+      };
 
       # https://discourse.nixos.org/t/setting-the-user-profile-image-under-gnome/36233
       system.activationScripts =
