d09e62968daf117f96ac5ac82b3bed4c1c68e08e
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-12-28 20:18:35 +0100

    gnome: openbar gtk theming options (but still not enabled)

--- modules/home/linux/desktop/gnome/extensions/open-bar.nix
+++ modules/home/linux/desktop/gnome/extensions/open-bar.nix
@@ -41,13 +41,14 @@ modules.mkModule inputs ./open-bar.nix {
         apply-accent-shell = false;
         apply-all-shell = false;
         # gtk/flatpak
-        # headerbar-hint = 0;
-        # sidebar-hint = 0;
-        # card-hint = 0;
-        # view-hint = 0;
-        # window-hint = 0;
-        # winbwidth = 0.0; # window border width
-        # corner-radius = true;
+        headerbar-hint = 0;
+        sidebar-hint = 0;
+        card-hint = 0;
+        view-hint = 0;
+        window-hint = 0;
+        winbalpha = 0.0;
+        winbwidth = 0.0;
+        corner-radius = true;
         apply-gtk = false;
         apply-flatpak = false;
       }
