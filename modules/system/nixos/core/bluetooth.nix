f40fea075a10b0c8e562557118bb4bee037130fb
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-01-04 10:29:27 +0100

    bluetooth: optional fast connectable (and false by default)

--- modules/system/nixos/core/bluetooth.nix
+++ modules/system/nixos/core/bluetooth.nix
@@ -1,28 +1,33 @@
 { lib, ... }@inputs:
 with lib.custom;
 modules.mkModule inputs ./bluetooth.nix {
-  config = {
-    # https://nixos.wiki/wiki/Bluetooth
-    hardware.bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-      settings = {
-        General = {
-          # Shows battery charge of connected devices on supported
-          # Bluetooth adapters. Defaults to 'false'.
-          Experimental = true;
-          # When enabled other devices can connect faster to us, however
-          # the tradeoff is increased power consumption. Defaults to
-          # 'false'.
-          FastConnectable = true;
-        };
-        Policy = {
-          # Enable all controllers when they are found. This includes
-          # adapters present on start as well as adapters that are plugged
-          # in later on. Defaults to 'true'.
-          AutoEnable = true;
+  options = {
+    fastConnectable = lib.mkEnableOption "fast connectable mode (increased idle power consumption)";
+  };
+  config =
+    { self, ... }:
+    {
+      # https://nixos.wiki/wiki/Bluetooth
+      hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+        settings = {
+          General = {
+            # Shows battery charge of connected devices on supported
+            # Bluetooth adapters. Defaults to 'false'.
+            Experimental = true;
+            # When enabled other devices can connect faster to us, however
+            # the tradeoff is increased power consumption. Defaults to
+            # 'false'.
+            FastConnectable = self.fastConnectable;
+          };
+          Policy = {
+            # Enable all controllers when they are found. This includes
+            # adapters present on start as well as adapters that are plugged
+            # in later on. Defaults to 'true'.
+            AutoEnable = true;
+          };
         };
       };
     };
-  };
 }
