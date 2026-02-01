67ed9813f558e218ef0fcc1e27f67e30ab67ed2d
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-02-01 23:26:30 +0100

    plasma system config

--- modules/system/nixos/desktop/plasma.nix
+++ modules/system/nixos/desktop/plasma.nix
@@ -1,11 +1,32 @@
 { lib, pkgs, ... }@inputs:
 with lib.custom;
 modules.mkModule inputs ./plasma.nix {
-  config = {
-    # https://wiki.nixos.org/wiki/KDE
-    services.desktopManager.plasma6.enable = true;
-    environment.systemPackages = with pkgs.kdePackages; [
-      sddm-kcm
-    ];
+  options = {
+    excludePackages = lib.mkOption {
+      type = with lib.types; listOf package;
+      default = [ ];
+      description = "List of kde packages to exclude.";
+    };
   };
+  config =
+    { self, ... }:
+    {
+      # https://wiki.nixos.org/wiki/KDE
+      # https://nixos.wiki/wiki/KDE
+      services.desktopManager.plasma6.enable = true;
+      environment = with pkgs.kdePackages; {
+        systemPackages = [
+          kcalc
+          kcharselect
+          kclock
+          kcolorchooser
+          kolourpaint
+          ksystemlog
+          isoimagewriter
+          sddm-kcm
+          partitionmanager
+        ];
+        plasma6 = { inherit (self) excludePackages; };
+      };
+    };
 }
