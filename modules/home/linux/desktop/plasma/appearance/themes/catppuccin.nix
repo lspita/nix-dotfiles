ed893a4049b29566da0d9c2bad7e282a4ffc4e59
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-02-04 08:52:55 +0100

    plasma: catppuccin optional reset

--- modules/home/linux/desktop/plasma/appearance/themes/catppuccin.nix
+++ modules/home/linux/desktop/plasma/appearance/themes/catppuccin.nix
@@ -1,25 +1,37 @@
 { lib, pkgs, ... }@inputs:
 with lib.custom;
 modules.mkModule inputs ./catppuccin.nix {
-  config = {
-    home.packages = with pkgs; [
-      (catppuccin-kde.override {
-        flavour = [
-          "mocha"
-          "latte"
-        ];
-        accents = [ "sapphire" ];
-        winDecStyles = [ "classic" ];
-      })
-      catppuccin-cursors.mochaLight
-      catppuccin-cursors.mochaSapphire
-      catppuccin-cursors.latteDark
-      catppuccin-cursors.latteSapphire
-    ];
-    programs.plasma.workspace = {
-      theme = "default"; # use breeze and override colors
-      colorScheme = "CatppuccinMochaSapphire";
-      cursor.theme = "catppuccin-mocha-sapphire-cursors";
-    };
+  options = {
+    reset = modules.mkEnableOption (!(packages.isInstalled inputs "koi")) "Reset the default profile";
   };
+  config =
+    { self, ... }:
+    {
+      home.packages = with pkgs; [
+        (catppuccin-kde.override {
+          flavour = [
+            "mocha"
+            "latte"
+          ];
+          accents = [ "sapphire" ];
+          winDecStyles = [ "classic" ];
+        })
+        catppuccin-cursors.mochaLight
+        catppuccin-cursors.mochaSapphire
+        catppuccin-cursors.latteDark
+        catppuccin-cursors.latteSapphire
+      ];
+      programs.plasma.workspace = {
+        theme = "default"; # use breeze and override colors
+      }
+      // (
+        if self.reset then
+          {
+            colorScheme = "CatppuccinMochaSapphire";
+            cursor.theme = "catppuccin-mocha-sapphire-cursors";
+          }
+        else
+          { }
+      );
+    };
 }
