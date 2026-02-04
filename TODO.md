27f4df74517b13f92515c8eb67185f2086a517a6
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-02-04 17:54:42 +0100

    european english locales

--- TODO.md
+++ TODO.md
@@ -3,5 +3,4 @@
 - [ ] Power profiles (hybernate after 2 hours)
 - [ ] Wallpapers
 - [ ] Profile image
-- [ ] Kwrite "Allow scrolling past end of document"
 - [ ] Meta+Arrows shortcuts for windows
--- vars.nix
+++ vars.nix
@@ -52,12 +52,12 @@
           "it_IT"
         ];
       extraSettings =
-        # { [string] = string }?: extra locales
+        # { [string] = string }?: extra locales. en_IE is the most similar to EU english
         {
           LC_ADDRESS = "it_IT.UTF-8";
           LC_IDENTIFICATION = "it_IT.UTF-8";
-          LC_MEASUREMENT = "it_IT.UTF-8";
-          LC_MONETARY = "it_IT.UTF-8";
+          LC_MEASUREMENT = "en_IE.UTF-8";
+          LC_MONETARY = "en_IE.UTF-8";
           LC_NAME = "it_IT.UTF-8";
           LC_NUMERIC = "en_IE.UTF-8";
           LC_PAPER = "it_IT.UTF-8";
