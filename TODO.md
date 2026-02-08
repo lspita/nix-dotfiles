436dccfd19abcef1e35e3f199ac3a944d1d454b3
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-02-08 23:10:23 +0100

    kde: accounts integration (google not working)

--- TODO.md
+++ TODO.md
@@ -3,6 +3,6 @@
 - [ ] Power profiles (hybernate after 2 hours)
 - [ ] Wallpapers
 - [ ] Profile image
-- [ ] Fix online accounts
+- [?] Fix online accounts
 - [ ] Night light
 - [ ] Starship prompt
--- modules/system/nixos/desktop/plasma.nix
+++ modules/system/nixos/desktop/plasma.nix
@@ -8,6 +8,7 @@
 with lib.custom;
 modules.mkModule inputs ./plasma.nix {
   options = {
+    integrations.enable = modules.mkEnableOption true "accounts integration";
     excludePackages = lib.mkOption {
       type = with lib.types; listOf package;
       default = optionals.ifNotNull [ ] (with pkgs.kdePackages; [
@@ -18,38 +19,45 @@ modules.mkModule inputs ./plasma.nix {
   };
   config =
     { self, ... }:
-    {
-      # https://wiki.nixos.org/wiki/KDE
-      # https://nixos.wiki/wiki/KDE
-      services.desktopManager.plasma6.enable = true;
-      environment = with pkgs.kdePackages; {
-        systemPackages = [
-          kcalc
-          kcharselect
-          kclock
-          kcolorchooser
-          kolourpaint
-          ksystemlog
-          isoimagewriter
-          partitionmanager
+    lib.mkMerge [
+      {
+        # https://wiki.nixos.org/wiki/KDE
+        # https://nixos.wiki/wiki/KDE
+        services.desktopManager.plasma6.enable = true;
+        environment = with pkgs.kdePackages; {
+          systemPackages = [
+            kcalc
+            kcharselect
+            kclock
+            kcolorchooser
+            kolourpaint
+            ksystemlog
+            isoimagewriter
+            partitionmanager
+          ]
+          ++ (
+            if config.services.displayManager.sddm.enable then
+              [
+                # sddm settings integration
+                sddm-kcm
+              ]
+            else
+              [ ]
+          );
+          plasma6 = {
+            excludePackages = self.excludePackages;
+          };
+        };
+      }
+      (lib.mkIf self.integrations.enable {
+        environment.systemPackages = with pkgs.kdePackages; [
           # online accounts integration
           kaccounts-integration
           kaccounts-providers
-          kio-extras
           signond
-        ]
-        ++ (
-          if config.services.displayManager.sddm.enable then
-            [
-              # sddm settings integration
-              sddm-kcm
-            ]
-          else
-            [ ]
-        );
-        plasma6 = {
-          excludePackages = self.excludePackages;
-        };
-      };
-    };
+          signon-kwallet-extension
+          kio-gdrive
+        ];
+      })
+    ];
 }
