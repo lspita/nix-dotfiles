8f8b30dc4b65d807c69d5236136dbc6f00d630cd
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-02-13 18:22:25 +0100

    android: only home config

--- hosts/clank/system.nix
+++ hosts/clank/system.nix
@@ -3,7 +3,6 @@
   custom = {
     core.enableDefaults = true;
     services.enableDefaults = true;
-    development.enableDefaults = true;
     nixos = {
       core.enableDefaults = true;
       services.enableDefaults = true;
--- modules/system/development/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }@inputs:
-with lib.custom;
-modules.mkDefaultsModule inputs ./. {
-  config =
-    { setDefaultSubconfig, ... }:
-    setDefaultSubconfig {
-      android.enable = true;
-    };
-}
