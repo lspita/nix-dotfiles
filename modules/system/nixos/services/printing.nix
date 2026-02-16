243f927acc87c6af6f135c59b9f775647ab981bb
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-02-16 21:19:56 +0100

    automatic printing discovery

--- modules/system/nixos/services/printing.nix
+++ modules/system/nixos/services/printing.nix
@@ -1,7 +1,30 @@
-{ lib, ... }@inputs:
+{ lib, pkgs, ... }@inputs:
 with lib.custom;
 modules.mkModule inputs ./printing.nix {
-  config = {
-    services.printing.enable = true; # cups
+  options = {
+    drivers = lib.mkOption {
+      type = with lib.types; listOf package;
+      default = with pkgs; [
+        cups-filters
+        cups-browsed
+      ];
+      description = "List of printer drivers to install";
+    };
   };
+  config =
+    { self, ... }:
+    {
+      # https://wiki.nixos.org/wiki/Printing
+      services = {
+        printing = {
+          inherit (self) drivers;
+          enable = true;
+        };
+        avahi = {
+          enable = true;
+          nssmdns4 = true;
+          openFirewall = true;
+        };
+      };
+    };
 }
