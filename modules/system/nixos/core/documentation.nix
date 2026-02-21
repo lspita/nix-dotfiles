fceece5e05d6618accbc5eb33805c0c928537fb8
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-02-21 12:19:21 +0100

    more man pages

--- modules/system/nixos/core/documentation.nix
+++ modules/system/nixos/core/documentation.nix
@@ -1,9 +1,18 @@
-{ lib, ... }@inputs:
+{ lib, pkgs, ... }@inputs:
 with lib.custom;
 modules.mkModule inputs ./documentation.nix {
   config = {
+    # https://wiki.nixos.org/wiki/Man_pages
+    # https://search.nixos.org/options?channel=unstable&query=documentation
+    environment.systemPackages = with pkgs; [
+      man-pages
+      man-pages-posix
+    ];
     documentation = {
       enable = true;
+      nixos.enable = true;
+      info.enable = true;
+      doc.enable = true;
       man.enable = true;
       dev.enable = true;
     };
