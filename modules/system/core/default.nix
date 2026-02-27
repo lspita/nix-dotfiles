4ec2f2d1242b23b42f35fb30469eee49d09b9192
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-02-27 19:39:37 +0100

    nix: cuda cache

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }@inputs:
+{ lib, vars, ... }@inputs:
 with lib.custom;
 modules.mkDefaultsModule inputs ./. {
   config =
@@ -6,12 +6,19 @@ modules.mkDefaultsModule inputs ./. {
     setDefaultSubconfig {
       nix = {
         enable = true;
-        extraSubstituters = [
-          {
-            url = "https://nix-community.cachix.org/";
-            key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
-          }
-        ];
+        extraSubstituters =
+          # https://wiki.nixos.org/wiki/CUDA#Setting_up_CUDA_Binary_Cache
+          (lib.lists.optional vars.nixpkgs.config.cudaSupport {
+            url = "https://cache.nixos-cuda.org";
+            key = "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=";
+          })
+          ++ [
+            # https://app.cachix.org/cache/nix-community
+            {
+              url = "https://nix-community.cachix.org/";
+              key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+            }
+          ];
       };
       user.enable = true;
     };
