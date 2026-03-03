98c568f6b69c352df015e52870eba9eb329b7989
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-03-03 12:02:41 +0100

    plasma: explicit override config

--- modules/home/linux/desktop/plasma/settings.nix
+++ modules/home/linux/desktop/plasma/settings.nix
@@ -19,6 +19,7 @@ modules.mkModule inputs ./settings.nix {
     { self, ... }:
     {
       programs.plasma = {
+        overrideConfig = false;
         workspace = {
           enableMiddleClickPaste = false;
           splashScreen.theme = "org.kde.breeze.desktop";
