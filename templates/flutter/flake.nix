5e5b3c2f0d7ecb1eadda219eea583f731320b193
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-03-15 17:05:25 +0100

    templates: flutter

---
+++ templates/flutter/flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
+        };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              # nix
+              nixd
+              nil
+              nixfmt
+              # flutter
+              flutter
+              dart
+              cmake
+              # java
+              jdk
+              gradle
+            ];
+            shellHook = ''
+              set -a
+              source .env 2>/dev/null
+              set +a
+            '';
+          };
+      }
+    );
+}
