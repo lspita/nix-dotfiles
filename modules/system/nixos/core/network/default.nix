d08958228fbde0fd01b2d1f07aaeaea8e781c829
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-04-13 09:32:58 +0200

    nm: use default backend

--- modules/system/nixos/core/network/default.nix
+++ modules/system/nixos/core/network/default.nix
@@ -6,10 +6,10 @@ modules.mkDefaultsModule inputs ./. {
     lib.mkMerge [
       (setDefaultSubconfig {
         networkmanager.enable = true;
-        iwd = {
-          enable = true;
-          enableNMWifiBackend = true;
-        };
+        # iwd = {
+        #   enable = true;
+        #   enableNMWifiBackend = true;
+        # };
       })
       {
         hardware.wirelessRegulatoryDatabase = true;
